efcddf00d7db9f8b4b250a1cd4128091f98eee54
gerardojgarcia <gerardojgarcia@pm.me>
2022-08-08 17:56:23 -0700

    project final touches - footer and plan card section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -33,7 +33,7 @@
 
 
 <div class="footer m-0 bg-gray-200">
-    <div class="footer-container flex flex-col w-full mx-4 justify-center lg:flex-row lg:px-24 lg:py-8 ">
+    <div class="footer-container flex flex-col w-full mx-4 justify-center items-center lg:flex-row lg:px-24 lg:py-8 ">
     
         <div class="footer-text flex flex-col w-full my-4">
             <strong class="text-2xl text-left text-blue-400 my-4 lg:text-4xl">Thank you for supporting us!</strong>
@@ -41,26 +41,26 @@
         </div>
     
     
-        <div class="social-icons flex flex-row align-left my-4 h-16 justify-center items-center lg:pr-8   ">
+        <div class="social-icons flex flex-row align-left my-4 h-16 justify-center items-center lg:pr-64   ">
     
-            <div class="icon-box  bg-blue-500  flex flex-col place-center items-center justify-center" :style="{background: facebookColor}">
+            <div class="icon-box  bg-blue-500  flex flex-col place-center items-center justify-center cursor-pointer" :style="{background: facebookColor}">
         <i class="fa-brands fa-facebook-f"></i>
     
     
             </div>
-                    <div class="icon-box  bg-blue-500  flex flex-col place-center items-center justify-center" :style="{background: twitterColor}">
+                    <div class="icon-box  bg-blue-500  flex flex-col place-center items-center justify-center cursor-pointer" :style="{background: twitterColor}">
         <i class="fa-brands fa-twitter"></i>
     
     
             </div>
     
-                    <div class="icon-box  bg-blue-500  flex flex-col place-center items-center justify-center" :style="{background: githubColor}">
+                    <div class="icon-box  bg-blue-500  flex flex-col place-center items-center justify-center cursor-pointer" :style="{background: githubColor}">
     <i class="fa-brands fa-github"></i>
     
     
             </div>
     
-                    <div class="icon-box bg-blue-500  flex flex-col place-center items-center justify-center" :style="{background: dribbbleColor}">
+                    <div class="icon-box bg-blue-500  flex flex-col place-center items-center justify-center cursor-pointer" :style="{background: dribbbleColor}">
     <i class="fa-brands fa-dribbble"></i>
     
     
